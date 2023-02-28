f955151f3e43b0d66e6b37e0c515405991e44fb5
Administrator <120100729+beckybbk@users.noreply.github.com>
2023-02-28 21:50:34 +0900

    게임0228

---
+++ Game_bk/Assets/Script/GameEvent.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEvent : MonoBehaviour
+{
+    public static GameEvent instance;
+    public event Action onDoorEnter;
+
+
+    private void Awake()
+    {
+        
+        if(instance==null)
+        {
+            instance = this;
+
+        }
+    }
+
+    public void DoorTriggerEnter()
+    {
+        if(onDoorEnter != null)
+        {
+            onDoorEnter();
+
+        }
+    }
+        
+}
